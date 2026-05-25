200f9a5122722220ca4e642f1d37a61431749e14
Chamindukeshan <Chamindukeshankeshan233@gmail.com>
2026-05-25 23:39:05 +0530

    Update SESSION_ID and AUTO_READ_STATUS defaults

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "0N8EmTQb#OsWQQxqXK95F1zJKyxrgwNg57s0H18wtfYTePeXjtus",
+SESSION_ID: process.env.SESSION_ID || "BEUzlbDJ#AOd_LBCqc9AF5jHp8y2A2cA0cDNor13ed4CRB2h2dqQ",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 };
