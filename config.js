564c0b6128f94bcd2740db56887def21350c5b0d
Chamindukeshan <Chamindukeshankeshan233@gmail.com>
2026-08-24 20:19:40 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "BEUzlbDJ#AOd_LBCqc9AF5jHp8y2A2cA0cDNor13ed4CRB2h2dqQ",
+SESSION_ID: process.env.SESSION_ID || "lJFTVYII#jARFHY9tyibgqV0ThO9qHCiBCfRQmmui8HiJJZT-mCA",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", 
 };
